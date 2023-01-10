d2ae82cac12c8d831cd5b29342fcbefd2f3a4eef
Hiro Protagonist <shd327@gmail.com>
2023-01-10 07:17:48 -0700

    adding scroll feature to nav

--- react-portfolio/src/components/navbar/Navbar.js
+++ react-portfolio/src/components/navbar/Navbar.js
@@ -1,9 +1,24 @@
-import React from 'react'
+import React, { useEffect } from 'react';
+import { useState } from 'react';
 import { Navbar, Container } from "react-bootstrap";
 
 export default function Navbar() {
+    const [activeLink, setActiveLink] = useState('home')
+    const [scolled, setScrolled] = useState(false)
+    useEffect(() => {
+        const onScroll = () => {
+            if (window.scrollY > 50) {
+                setScrolled(true)
+            }
+            else {
+                setScrolled(false)
+            }
+            window.addEventListener('scroll', onScroll)
+            return () => window.removeEventListener('scroll', onScroll)
+        }
+    }, [])
     return (
-        <Navbar bg="light" expand="lg">
+        <Navbar bg="light" expand="lg" className={scrolled ? 'scrolled' : ""}>
             <Container>
                 <Navbar.Brand href="#home">
                     <img src={''} alt="logo" />
@@ -11,20 +26,19 @@ export default function Navbar() {
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="me-auto">
-                        <Nav.Link href="#home">Home</Nav.Link>
-                        <Nav.Link href="#link">Link</Nav.Link>
-                        <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-                            <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                            <NavDropdown.Item href="#action/3.2">
-                                Another action
-                            </NavDropdown.Item>
-                            <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-                            <NavDropdown.Divider />
-                            <NavDropdown.Item href="#action/3.4">
-                                Separated link
-                            </NavDropdown.Item>
-                        </NavDropdown>
+                        <Nav.Link href="#home" className={active === 'home' ? 'active navbar-link' : 'navbar-link'}>Home</Nav.Link>
+                        <Nav.Link href="#Skills">Skills</Nav.Link>
+                        <Nav.Link href="#Projects">Projects</Nav.Link>
                     </Nav>
+                    <span className='navbar-text'>
+                        <div className='social-icon'>
+                            <a href='#'><img src={''} alt="" /> </a>
+                            <a href='#'><img src={''} alt="" /> </a>
+                            <a href='#'><img src={''} alt="" /> </a>
+                        </div>
+                        <button className='' onClick={() => console.log('Contact us form')}><span>Let's Connect</span></button>
+                    </span>
+
                 </Navbar.Collapse>
             </Container>
         </Navbar>
