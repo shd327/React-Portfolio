adc8e77edd7f49ec30d6caa05920c07cc61586bb
Hiro Protagonist <shd327@gmail.com>
2023-01-10 08:34:15 -0700

    adding useeffect for changing text

--- react-portfolio/src/components/Banner/Banner.js
+++ react-portfolio/src/components/Banner/Banner.js
@@ -1,6 +1,9 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { Container, Row, Col } from 'react-bootstrap'
-
+import bannerImg from '../../assets/img/banner.jpg'
+import { useState, useEffect } from 'react';
+const [loopNum, setloopNum] = useState(0);
+const [isDeleting, setIsDeleting] = useState(false);
 export default function Banner() {
     return (
         <section className='banner' id='home'>
@@ -8,6 +11,12 @@ export default function Banner() {
                 <Row className='align-items-center'>
                     <Col xs={12} md={6} xl={7}>
                         <span className='tagline'>Welcome to my Portfolio</span>
+                        <h1>{`Hi I'm Sid Desai`} <span className='wrap'>Web Developer</span></h1>
+                        <p>I love pizza very much pizza makes me stronger than stinky old broccoli</p>
+                        <button onClick={() => console.log('connect')}>Let's Connect</button>
+                    </Col>
+                    <Col xs={12} md={6} xl={5}>
+                        <img src={bannerImg} alt="Header Img"></img>
                     </Col>
                 </Row>
 
