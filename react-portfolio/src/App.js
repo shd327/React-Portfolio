c8cbf24ff8ee24e4dcf05936d27476eefc58e689
Hiro Protagonist <shd327@gmail.com>
2023-01-10 09:14:15 -0700

    couldnt get typewriter to work but added banner to app.js

--- react-portfolio/src/App.js
+++ react-portfolio/src/App.js
@@ -1,10 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import NavBar from './components/NavBar/NavBar';
+import Banner from './components/Banner/Banner'
 import 'bootstrap/dist/css/bootstrap.min.css';
 function App() {
   return (
-    <NavBar />
+    <>
+      <NavBar />
+      <Banner />
+    </>
   );
 }
 
--- react-portfolio/src/components/Banner/Banner.js
+++ react-portfolio/src/components/Banner/Banner.js
@@ -1,9 +1,13 @@
 import React, { useEffect } from 'react'
 import { Container, Row, Col } from 'react-bootstrap'
-import bannerImg from '../../assets/img/banner.jpg'
-import { useState, useEffect } from 'react';
-const [loopNum, setloopNum] = useState(0);
-const [isDeleting, setIsDeleting] = useState(false);
+import bannerImg from '../../assets/img/dude.png'
+
+// import Typewriter from 'typewriter-effect/dist/core';
+
+// new Typewriter('#typewriter', {
+//     strings: ['Hello', 'World'],
+//     autoStart: true,
+// });
 export default function Banner() {
     return (
         <section className='banner' id='home'>
@@ -11,7 +15,7 @@ export default function Banner() {
                 <Row className='align-items-center'>
                     <Col xs={12} md={6} xl={7}>
                         <span className='tagline'>Welcome to my Portfolio</span>
-                        <h1>{`Hi I'm Sid Desai`} <span className='wrap'>Web Developer</span></h1>
+                        <h1>{`Hi I'm Sid Desai`} <span className='wrap'></span></h1>
                         <p>I love pizza very much pizza makes me stronger than stinky old broccoli</p>
                         <button onClick={() => console.log('connect')}>Let's Connect</button>
                     </Col>
